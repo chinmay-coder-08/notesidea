10a9205276014d69a0b6201852a9c9dcd0aa2b38
chinmay-coder-08 <chinmaybhogaonkarb@outlook.com>
2024-09-25 15:43:39 +0530

    Added navbar

--- index.php
+++ index.php
@@ -5,8 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NotesIdea</title>
     <link rel="stylesheet" href="./css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <div  class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href=""><img alt="logo" src="./static/images/logo.png" class="logo" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="" style="color: black;">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="" style="color: black;">My Notes</a>
+                        </li>
+                        <li class="nav-item">
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="" style="color: black;">Add Note</a>
+                        </li>
+              
+                    </ul>
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="" style="color: black;">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="" style="color: black;">Login</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    
     <h1 class="first_heading">NotesIdea</h1>
     <h2 class="first_heading">You can save your notes on cloud</h1>
     <h3 class="first_heading">
@@ -15,4 +51,5 @@
     ?>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
