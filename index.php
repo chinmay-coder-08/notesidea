0205585229b5bc3b468a79fd822fba5398a02809
chinmay-coder-08 <chinmaybhogaonkarb@outlook.com>
2024-09-25 14:32:06 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NotesIdea</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <h1 class="first_heading">NotesIdea</h1>
+    <h2 class="first_heading">You can save your notes on cloud</h1>
+    <h3 class="first_heading">
+    <?php
+        echo "This is PHP script";
+    ?>
+    </div>
+</body>
+</html>
